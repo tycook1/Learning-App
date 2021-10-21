a648678c9a0a72b2d9ec99602c0f31213ac04e59
Tyler Cook <tycook1@gmail.com>
2021-10-21 16:11:20 -0600

    - rename content view - organize files into folders - add content model - set content model as environment object

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Tyler Cook on 10/21/21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  LearningApp
 //
 //  Created by Tyler Cook on 10/21/21.
@@ -7,15 +7,19 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
+        
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
