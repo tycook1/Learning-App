0bd7b967c4a04225daec966a7202445db301ab56
Tyler Cook <tycook1@gmail.com>
2021-10-25 13:44:00 -0600

    - fix bug that caused crash when navigating from one module to the next

--- LearningApp/Views/ContentViewRow.swift
+++ LearningApp/Views/ContentViewRow.swift
@@ -12,6 +12,16 @@ struct ContentViewRow: View {
     @EnvironmentObject var model: ContentModel
     var index: Int
     
+    var lesson: Lesson {
+        
+        if model.currentModule != nil && index < model.currentModule!.content.count {
+            return model.currentModule!.content.lessons[index]
+        }
+        else {
+            return Lesson(id: 0, title: "", video: "", duration: "", explanation: "")
+        }
+    }
+    
     var body: some View {
         
         let lesson = model.currentModule!.content.lessons[index]
